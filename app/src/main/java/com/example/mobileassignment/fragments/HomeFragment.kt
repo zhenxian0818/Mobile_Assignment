4ec007ec347f098b9097ac1a08eddd26a0fc49af
zhenxian818@gmail.com <zhenxian818@gmail.com>
2022-10-03 22:18:14 +0800

    delete unused import

--- app/src/main/java/com/example/mobileassignment/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/mobileassignment/fragments/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.mobileassignment.QuizTitleActivity
 import com.example.mobileassignment.R
 
